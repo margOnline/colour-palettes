c6916892b2c0a7aa8e28eac2986523e3dbbeca16
Margo Urey <peterandmargo@blueyonder.co.uk>
2022-05-15 22:37:57 +0100

    First exercise - ColorBox

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View, SafeAreaView, StyleSheet } from 'react-native';
+import ColorBox from './components/ColorBox'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.heading}>Some boxes of different colours</Text>
+        </View>
+        <ColorBox colorName="Cyan" hexCode="#2aa198" />
+        <ColorBox colorName="Blue" hexCode="#268bd2" />
+        <ColorBox colorName="Magenta" hexCode="#d33682" />
+        <ColorBox colorName="Orange" hexCode="#cb4b16" />
+      </View>
+    </SafeAreaView>
+  )
+
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 10,
   },
+  heading: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10
+  }
 });
+
+export default App;
